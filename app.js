05d5720c674d122aa989b761a5f96d7dad5d3eba
Abriel Cleaver <abrielcleaver@gmail.com>
2021-08-27 15:26:27 -0700

    adds JS for input boxes

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 // import functions
 
 // reference needed DOM elements
+const inputVerb1 = document.getElementById('input-verb-1'); 
+console.log(inputVerb1);
+
+const submitButton = document.getElementById('submit-button');
+console.log(submitButton);
+
+const verb1 = document.getElementById('verb-1');
+console.log(verb1);
+
+submitButton.addEventListener('click', () => {
+    const newVerb1 = inputVerb1.value; 
+    verb1.textContent = newVerb1;
+    console.log(newVerb1);
+});
+
 
 
 // set event listeners 
